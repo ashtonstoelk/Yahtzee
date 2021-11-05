ec5d1c047cbbd6d2d204f1e5dda22b96dd2f6b5a
Zeldafreak505 <loganhannen@gmail.com>
2021-11-05 13:11:27 -0500

    Create Yahtzee.py

---
+++ Yahtzee.py
@@ -0,0 +1,16 @@
+''' Final Assignment 
+
+Our Yahtzee assignment can be done here
+
+'''
+def yahtzee():
+    #code here
+    return
+
+def diceRolling():
+    #code here
+    return
+    
+def checkResults():
+    #code here
+    return
